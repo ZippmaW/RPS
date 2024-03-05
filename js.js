3072d3efa3d9644fc38a8b15d9fd11aebe2b9a54
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-05 15:35:51 +0100

    Create function getComputerChoice
    create a function that randomly returns Rock, Paper or Scissors

---
+++ js.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3 + 1)) {
+    case 1:
+      console.log("Rock");
+      break;
+    case 2:
+      console.log("Paper");
+      break;
+    case 3:
+      console.log("Scissors");
+      break;
+  }
+}
