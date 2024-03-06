24e942a14942301ba259a524efe3118ebac237f1
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-06 13:52:45 +0100

    Create function called playGame()
    Write a function playGame() that loops the function playRound() 5x times, keeps the scores in separate variables and announces the winner at the end of the loop. Modify previous function playerSelection() so it prompts the user to give an appropriate input.

--- js.js
+++ js.js
@@ -3,19 +3,29 @@
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
+      console.log("Computer chose: rock");
       return "rock";
-      break;
     case 2:
+      console.log("Computer chose: paper");
       return "paper";
-      break;
     case 3:
+      console.log("Computer chose: scissors");
       return "scissors";
-      break;
   }
 }
 
-function getPlayerChoice(choice) {
-  return choice.toLowerCase();
+function getPlayerChoice() {
+  let selection = prompt("Please enter your selection").toLowerCase();
+  while (true) {
+    if (["rock", "paper", "scissors"].includes(selection)) {
+      console.log(`Player chose: ${selection}`);
+      return selection;
+    } else {
+      selection = prompt(
+        "Invalid input, please enter a string between rock, paper or scissors."
+      );
+    }
+  }
 }
 
 function playRound(computer, player) {
@@ -25,11 +35,43 @@ function playRound(computer, player) {
     (computer === "scissors" && player === "paper")
   ) {
     console.log("Computer wins");
+    return "NPC";
   } else if (computer === player) {
     console.log("Draw");
+    return "DRAW";
   } else {
     console.log("Player wins!");
+    return "PLAYER";
   }
 }
 
-playRound(getComputerChoice(), "scissors");
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    switch (playRound(getComputerChoice(), getPlayerChoice())) {
+      case "NPC":
+        computerScore += 1;
+        break;
+      case "DRAW":
+        computerScore += 1;
+        playerScore += 1;
+        break;
+      case "PLAYER":
+        playerScore += 1;
+        break;
+    }
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+  }
+  if (playerScore > computerScore) {
+    console.log(`Player won the game with ${playerScore} !`);
+  }
+  if (computerScore > playerScore) {
+    console.log(`Computer won the game with ${computerScore} !`);
+  } else if (playerScore === computerScore) {
+    console.log(`It is a draw!`);
+  }
+}
+
+playGame();
