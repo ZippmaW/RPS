75eba4f2450c1d09f7c35227085bbc109c801368
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-06 09:32:31 +0100

    Create a function that plays a single round
    Write a function that takes two parameters, computerSelection and playerSelection and declares the winner. PlayerSelection should be case-sensitive.

--- js.js
+++ js.js
@@ -3,13 +3,33 @@
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
-      console.log("Rock");
+      return "rock";
       break;
     case 2:
-      console.log("Paper");
+      return "paper";
       break;
     case 3:
-      console.log("Scissors");
+      return "scissors";
       break;
   }
 }
+
+function getPlayerChoice(choice) {
+  return choice.toLowerCase();
+}
+
+function playRound(computer, player) {
+  if (
+    (computer === "rock" && player === "scissors") ||
+    (computer === "paper" && player === "rock") ||
+    (computer === "scissors" && player === "paper")
+  ) {
+    console.log("Computer wins");
+  } else if (computer === player) {
+    console.log("Draw");
+  } else {
+    console.log("Player wins!");
+  }
+}
+
+playRound(getComputerChoice(), "scissors");
