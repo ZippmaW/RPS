e4d38417380a8cebf7d86f676b1d69e40419cde1
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-12 13:03:45 +0100

    Create three buttons for rock, paper, scissors and cleanup code
    Create three buttons with appropriate style and text content in javascript and clean the necessary code
    so it does not bring up error during development.

--- js.js
+++ js.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.append(rockBtn, paperBtn, scissorsBtn);
+});
+
+rockBtn.setAttribute("style", "width: 80px; height: 30px; margin-right: 10px;");
+rockBtn.textContent = "Rock";
+paperBtn.setAttribute(
+  "style",
+  "width: 80px; height: 30px; margin-right: 10px;"
+);
+paperBtn.textContent = "Paper";
+scissorsBtn.setAttribute("style", "width: 80px; height: 30px;");
+scissorsBtn.textContent = "Scissors";
+
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
@@ -15,17 +33,7 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let selection = prompt("Please enter your selection").toLowerCase();
-  while (true) {
-    if (["rock", "paper", "scissors"].includes(selection)) {
-      console.log(`Player chose: ${selection}`);
-      return selection;
-    } else {
-      selection = prompt(
-        "Invalid input, please enter a string between rock, paper or scissors."
-      );
-    }
-  }
+  let selection = "";
 }
 
 function playRound(computer, player) {
@@ -48,21 +56,20 @@ function playRound(computer, player) {
 function playGame() {
   let playerScore = 0;
   let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    switch (playRound(getComputerChoice(), getPlayerChoice())) {
-      case "NPC":
-        computerScore += 1;
-        break;
-      case "DRAW":
-        computerScore += 1;
-        playerScore += 1;
-        break;
-      case "PLAYER":
-        playerScore += 1;
-        break;
-    }
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
+  switch (playRound(getComputerChoice(), getPlayerChoice())) {
+    case "NPC":
+      computerScore += 1;
+      break;
+    case "DRAW":
+      computerScore += 1;
+      playerScore += 1;
+      break;
+    case "PLAYER":
+      playerScore += 1;
+      break;
+
+      console.log(`Player score: ${playerScore}`);
+      console.log(`Computer score: ${computerScore}`);
   }
   if (playerScore > computerScore) {
     console.log(`Player won the game with ${playerScore} !`);
@@ -73,5 +80,3 @@ function playGame() {
     console.log(`It is a draw!`);
   }
 }
-
-playGame();
